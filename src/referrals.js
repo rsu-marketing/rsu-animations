70a1f07c20085e0a69b5532e746c6db467a0583d
Kaster23 <francoierace.dev@gmail.com>
2025-01-08 00:30:51 -0300

    Update referrals.js

--- src/referrals.js
+++ src/referrals.js
@@ -116,19 +116,25 @@ gsap.fromTo(".referrals-hero-img", {
 
   //Animate How it works
 
-   gsap.fromTo(".c-referral-process__step", {
+  gsap.fromTo(".c-referral-process__step", 
+  {
     y: '5rem',
     opacity: 0
-  }, {
+  }, 
+  {
     y: '0rem',
     duration: 0.8,
     opacity: 1,
+    stagger: 0.3,
     scrollTrigger: {
-      trigger: '.c-referral-process__step',
-      start: 'bot 95%',
+      trigger: ".c-referral-process__step",
+      start: "top 85%",
+      end: "bottom top",
       toggleActions: "play none none reverse",
+      markers: false,
     }
-  });
+  }
+);
   
 
 
