c8ab7cf676f5cdbfc115fe7d30d3eacce589c4d1
Max Trofimchuk <trof.dev@gmail.com>
2025-09-24 23:40:13 +0300

    change toc endtrigger

--- src/blogtemplate.js
+++ src/blogtemplate.js
@@ -1,11 +1,9 @@
 //import gsap from "../dist/gsap.js";
-import { ScrollTrigger } from "./gsap/all.js";
-import { ScrollSmoother } from "./gsap/all.js";
-
+import { ScrollTrigger } from './gsap/all.js';
+import { ScrollSmoother } from './gsap/all.js';
 
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 function blogtemplate() {
-
   // smooth scroll
   document.addEventListener('DOMContentLoaded', () => {
     let smoother = ScrollSmoother.create({
@@ -13,7 +11,7 @@ function blogtemplate() {
       content: '.smooth-content',
       smooth: 1,
       smoothTouch: 0.1,
-      effects: true
+      effects: true,
     });
 
     // Sticky TOC sidebar
@@ -22,223 +20,218 @@ function blogtemplate() {
       ScrollTrigger.create({
         trigger: tocSidebar,
         start: 'top 20px',
-        endTrigger: '.smooth-content',
+        endTrigger: tocSidebar.parentElement,
         end: 'bottom 20px',
         pin: true,
         pinSpacing: false,
         anticipatePin: 1,
-        invalidateOnRefresh: true
+        invalidateOnRefresh: true,
       });
     }
 
     ScrollTrigger.refresh();
-
   });
 
   // refresh scrolltrigger
-  let links = document.querySelectorAll(".cc-refresh")
-  links.forEach(link => {
+  let links = document.querySelectorAll('.cc-refresh');
+  links.forEach((link) => {
     link.addEventListener('click', () => {
       setTimeout(() => {
-        ScrollTrigger.refresh()
-      }, 300)
-    })
-  })
+        ScrollTrigger.refresh();
+      }, 300);
+    });
+  });
 
   // BG nav dropdown open
 
-  document.addEventListener("DOMContentLoaded", function () {
-    const dropdowns = document.querySelectorAll(".c-dropdown");
-    const navBg = document.querySelector(".c-nav-bg-open");
+  document.addEventListener('DOMContentLoaded', function () {
+    const dropdowns = document.querySelectorAll('.c-dropdown');
+    const navBg = document.querySelector('.c-nav-bg-open');
     let lastOpenedDropdown = null;
 
     dropdowns.forEach((dropdown) => {
-      dropdown.addEventListener("click", (event) => {
+      dropdown.addEventListener('click', (event) => {
         event.stopPropagation();
 
         if (lastOpenedDropdown === dropdown) {
           // Close dropdown if click on same dropdown
           lastOpenedDropdown = null;
-          navBg.style.display = "none";
+          navBg.style.display = 'none';
         } else {
           // If another dropdown is clicked, close the others and open the new one.
           dropdowns.forEach((otherDropdown) => {
-            otherDropdown.classList.remove("open");
+            otherDropdown.classList.remove('open');
           });
-          dropdown.classList.add("open");
-          navBg.style.display = "block";
+          dropdown.classList.add('open');
+          navBg.style.display = 'block';
           lastOpenedDropdown = dropdown;
         }
       });
     });
 
-    document.addEventListener("click", (event) => {
-      if (!event.target.classList.contains("c-dropdown")) {
+    document.addEventListener('click', (event) => {
+      if (!event.target.classList.contains('c-dropdown')) {
         // If you click outside of all the dropdowns, close them all
         dropdowns.forEach((dropdown) => {
-          dropdown.classList.remove("open");
+          dropdown.classList.remove('open');
         });
-        navBg.style.display = "none";
+        navBg.style.display = 'none';
         lastOpenedDropdown = null; // Reinitialise the last open dropdown
       }
     });
 
-    navBg.addEventListener("click", (event) => {
+    navBg.addEventListener('click', (event) => {
       event.stopPropagation(); // Prevent click propagation to the outside
-      navBg.style.display = "none";
+      navBg.style.display = 'none';
       lastOpenedDropdown = null; // Reinitialise the last open dropdown
     });
   });
 
   // anim txt hero
 
-  gsap.fromTo(".c-template-blog-wrapper", {
-    y: '5rem',
-    opacity: 0
-  }, {
-    y: '0rem',
-    duration: 0.8,
-    delay: 0.3,
-    opacity: 1,
-    scrollTrigger: {
-      trigger: '.c-template-blog-wrapper',
-      start: 'bot 85%',
-      toggleActions: "play none none reverse",
+  gsap.fromTo(
+    '.c-template-blog-wrapper',
+    {
+      y: '5rem',
+      opacity: 0,
+    },
+    {
+      y: '0rem',
+      duration: 0.8,
+      delay: 0.3,
+      opacity: 1,
+      scrollTrigger: {
+        trigger: '.c-template-blog-wrapper',
+        start: 'bot 85%',
+        toggleActions: 'play none none reverse',
+      },
     }
-  });
+  );
 
   // MARQUEE ON SCROLL
 
-  gsap.to(".c-scroll-marquee-wrapper", {
+  gsap.to('.c-scroll-marquee-wrapper', {
     scrollTrigger: {
-      trigger: ".c-scroll-marquee-wrapper",
-      start: "top 100%",
+      trigger: '.c-scroll-marquee-wrapper',
+      start: 'top 100%',
       scrub: true,
     },
     xPercent: -20,
-    ease: "quart.easeOut",
+    ease: 'quart.easeOut',
   });
 
   // FOOTER CHART ANIM
 
-  gsap.to(".c-img-chart-footer.cc-one", {
+  gsap.to('.c-img-chart-footer.cc-one', {
     scrollTrigger: {
-      trigger: ".c-chart-footer-wrapper",
-      start: "top 100%",
-      end: "bottom -10%",
+      trigger: '.c-chart-footer-wrapper',
+      start: 'top 100%',
+      end: 'bottom -10%',
       scrub: true,
     },
     y: '-11rem',
-    ease: "quart.easeOut",
+    ease: 'quart.easeOut',
   });
 
-  gsap.to(".c-img-chart-footer.cc-two", {
+  gsap.to('.c-img-chart-footer.cc-two', {
     scrollTrigger: {
-      trigger: ".c-chart-footer-wrapper",
-      start: "top 100%",
-      end: "bottom -10%",
+      trigger: '.c-chart-footer-wrapper',
+      start: 'top 100%',
+      end: 'bottom -10%',
       scrub: true,
     },
     y: '-20rem',
-    ease: "quart.easeOut",
+    ease: 'quart.easeOut',
   });
 
-  gsap.to(".c-img-chart-footer.cc-three", {
+  gsap.to('.c-img-chart-footer.cc-three', {
     scrollTrigger: {
-      trigger: ".c-chart-footer-wrapper",
-      start: "top 100%",
-      end: "bottom -10%",
+      trigger: '.c-chart-footer-wrapper',
+      start: 'top 100%',
+      end: 'bottom -10%',
       scrub: true,
     },
     y: '-7rem',
-    ease: "quart.easeOut",
+    ease: 'quart.easeOut',
   });
 
-  gsap.to(".c-img-chart-footer.cc-four", {
+  gsap.to('.c-img-chart-footer.cc-four', {
     scrollTrigger: {
-      trigger: ".c-chart-footer-wrapper",
-      start: "top 100%",
-      end: "bottom -10%",
+      trigger: '.c-chart-footer-wrapper',
+      start: 'top 100%',
+      end: 'bottom -10%',
       scrub: true,
     },
     y: '-2rem',
-    ease: "quart.easeOut",
+    ease: 'quart.easeOut',
   });
 
-  gsap.to(".c-img-chart-footer.cc-five", {
+  gsap.to('.c-img-chart-footer.cc-five', {
     scrollTrigger: {
-      trigger: ".c-chart-footer-wrapper",
-      start: "top 100%",
-      end: "bottom -10%",
+      trigger: '.c-chart-footer-wrapper',
+      start: 'top 100%',
+      end: 'bottom -10%',
       scrub: true,
     },
     y: '-6rem',
-    ease: "quart.easeOut",
+    ease: 'quart.easeOut',
   });
 
-  gsap.to(".c-img-chart-footer.cc-six", {
+  gsap.to('.c-img-chart-footer.cc-six', {
     scrollTrigger: {
-      trigger: ".c-chart-footer-wrapper",
-      start: "top 100%",
-      end: "bottom -10%",
+      trigger: '.c-chart-footer-wrapper',
+      start: 'top 100%',
+      end: 'bottom -10%',
       scrub: true,
     },
     y: '-11rem',
-    ease: "quart.easeOut",
+    ease: 'quart.easeOut',
   });
 
   ScrollTrigger.matchMedia({
-
     // large
-    "(min-width: 992px)": function () {
+    '(min-width: 992px)': function () {
       // Marquee on scroll
 
-      gsap.to(".c-scroll-marquee-wrapper", {
+      gsap.to('.c-scroll-marquee-wrapper', {
         scrollTrigger: {
-          trigger: ".c-scroll-marquee-wrapper",
-          start: "top 100%",
+          trigger: '.c-scroll-marquee-wrapper',
+          start: 'top 100%',
           scrub: true,
         },
         xPercent: -20,
-        ease: "quart.easeOut",
+        ease: 'quart.easeOut',
       });
-
     },
     // medium
-    "(min-width: 767px) and (max-width: 991px)": function () {
+    '(min-width: 767px) and (max-width: 991px)': function () {
       // Marquee on scroll
 
-      gsap.to(".c-scroll-marquee-wrapper", {
+      gsap.to('.c-scroll-marquee-wrapper', {
         scrollTrigger: {
-          trigger: ".c-scroll-marquee-wrapper",
-          start: "top 100%",
+          trigger: '.c-scroll-marquee-wrapper',
+          start: 'top 100%',
           scrub: true,
         },
         xPercent: -20,
-        ease: "quart.easeOut",
+        ease: 'quart.easeOut',
       });
-
-
     },
     // small
-    "(max-width: 480px)": function () {
+    '(max-width: 480px)': function () {
       // Marquee on scroll
 
-      gsap.to(".c-scroll-marquee-wrapper", {
+      gsap.to('.c-scroll-marquee-wrapper', {
         scrollTrigger: {
-          trigger: ".c-scroll-marquee-wrapper",
-          start: "top 100%",
+          trigger: '.c-scroll-marquee-wrapper',
+          start: 'top 100%',
           scrub: true,
         },
         xPercent: -80,
-        ease: "quart.easeOut",
+        ease: 'quart.easeOut',
       });
-
-
     },
-  })
-
-
+  });
 }
 
-export default blogtemplate
+export default blogtemplate;
