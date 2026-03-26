603c8f2f7547bd85440ba3949e022771ba7fc162
Max Trofimchuk <trof.dev@gmail.com>
2026-03-26 23:08:30 +0200

    change start position for about page split–text

--- src/about.js
+++ src/about.js
@@ -130,7 +130,7 @@ function about() {
 
         ScrollTrigger.create({
           trigger: element,
-          start: 'top 80%',
+          start: 'top 95%',
           once: true,
           onEnter: () => {
             queue.push({ words: split.words });
